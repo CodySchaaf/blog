f68429c58bc774b20f3e1e650f023d8d45839ff0
CodySchaaf <schaaf@sigfig.com>
2020-08-15 22:29:48 -0700

    Updates about me

--- src/pages/about.js
+++ src/pages/about.js
@@ -22,10 +22,13 @@ const About = () => (
     <h1>About</h1>
     <StyledContainer>
       <p>
-        Hi! My name is Cody Schaaf. I’m a web developer, game maker, and technology enthusiast. I spend most of my time
-        these days bouncing between different JavaScript frameworks, but I’m pretty language agnostic. My specialties at
-        the moment are React and Redux, with a side of Angular when the project suits it. I enjoy nothing more than
-        learning new languages and frameworks. Feel free to contact me with questions/comments/anything.
+        Hi! My name is Cody Schaaf. I’m a web developer, mountain biker, and
+        general JavaScript enthusiast. I spend most of my time these days
+        working with React and TypeScript, but I also enjoy Svelte, Vue, and
+        Gatsby. I love creating engaging user experiences, especially with
+        complex JavaScript animations. I enjoy nothing more than learning new
+        languages and frameworks. Feel free to contact me with
+        questions/comments/anything.
       </p>
       <StyledImage />
     </StyledContainer>
